2cb75ad5e7cf223e128d9cf4156cfd3d01c0cf51
kiannaquines <kjgnaquines@gmail.com>
2026-04-12 21:54:14 -0700

    feat(frontend): add breadcrumbs, sidebar icons, navbar

--- pharmacy_saas/views.py
+++ pharmacy_saas/views.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse
 
 
 @login_required
 def dashboard(request):
-    return render(request, "pages/dashboard.html")
+    return render(
+        request,
+        "pages/dashboard.html",
+        {
+            "breadcrumbs": [
+                {"label": "Home", "url": reverse("dashboard")},
+                {"label": "Dashboard"},
+            ],
+        },
+    )
